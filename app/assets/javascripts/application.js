c7ab489af9348f3257aec5bb17d8b95a291af34b
김성인 <kimsungin5@gmail.com>
2021-12-08 09:53:35 +0000

    final!!!(로그인 회원가입 예쁘게 홈페이지 배너 돌아간다)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
 //= require popper
+//= require jquery
 //= require bootstrap
 //= require activestorage
 //= require turbolinks
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,6 @@ class Cart < ApplicationRecord
   belongs_to :pack
 	
   def verified_save
-	  cart = 
 	  sample_carts = user.carts # current_user replace
 	  
 	# 기존에 장바구니에 상품이 있는지 확인하는 과정
